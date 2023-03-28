b776c38e405b94187d672b497f25d6bc4313eae1
amirreza79ahmadi <amirreza79ahmadi@gmail.com>
2023-03-28 14:14:32 +0330

    mobile menu

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -2,6 +2,7 @@ import { Html, Head, Main, NextScript } from 'next/document'
 import Footer from 'components/footer/footer'
 import Header from 'components/header/header'
 import HeadMeta from 'components/head-meta/head-meta'
+import Menu from 'components/mobile-menu/menu'
 
 export default function Document() {
   return (
@@ -12,6 +13,7 @@ export default function Document() {
       <body className='home'>
         <div className='page-wrapper'>
           <Header/>
+          <Menu/>
             <Main/>
           <Footer/>
         </div>
