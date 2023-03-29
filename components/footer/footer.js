ad61704afd90210d83e8638b6edd4cbde4dc0a1b
amirreza79ahmadi <amirreza79ahmadi@gmail.com>
2023-03-29 18:11:34 +0330

    checked componet footer

--- components/footer/footer.js
+++ components/footer/footer.js
@@ -1,9 +1,10 @@
+import Link from 'next/link'
 import React from 'react'
 
 export default function Footer() {
   return (
     <>
-      <footer className="footer footer-dark appear-animate mt-10">
+      <footer className="footer footer-dark appear-animate mt-10 rtl">
           <div className="footer-newsletter bg-primary">
               <div className="container">
                   <div className="row justify-content-center align-items-center">
@@ -13,10 +14,8 @@ export default function Footer() {
                                   <i className="w-icon-envelop3"></i>
                               </div>
                               <div className="icon-box-content">
-                                  <h4 className="icon-box-title text-white text-uppercase font-weight-bold">Subscribe To
-                                      Our Newsletter</h4>
-                                  <p className="text-white">Get all the latest information on Events, Sales and Offers.
-                                  </p>
+                                  <h4 className="icon-box-title text-white text-uppercase font-weight-bold">مشترک شدن در خبرنامه ما</h4>
+                                  <p className="text-white">تمام آخرین اطلاعات در مورد رویدادها، فروش ها و پیشنهادات را دریافت کنید.</p>
                               </div>
                           </div>
                       </div>
@@ -24,9 +23,8 @@ export default function Footer() {
                           <form action="#" method="get"
                               className="input-wrapper input-wrapper-inline input-wrapper-rounded">
                               <input type="email" className="form-control mr-2 bg-white" name="email" id="email"
-                                  placeholder="Your E-mail Address" />
-                              <button className="btn btn-dark btn-rounded" type="submit">Subscribe<i
-                                      className="w-icon-long-arrow-right"></i></button>
+                                  placeholder="ایمیل خود را وارد کنید ..." />
+                              <button className="btn btn-dark btn-rounded" type="submit">ثبت<i className="w-icon-long-arrow-right"></i></button>
                           </form>
                       </div>
                   </div>
@@ -37,153 +35,67 @@ export default function Footer() {
                   <div className="row">
                       <div className="col-lg-4 col-sm-6">
                           <div className="widget widget-about">
-                              <a href="demo9.html" className="logo-footer">
-                                  <img src="assets/images/demos/demo9/footer-logo.png" alt="logo-footer" width="144"
-                                      height="45" />
-                              </a>
+                              <Link href="/" className="logo-footer">
+                                  <img src="assets/images/demos/demo9/footer-logo.png" alt="logo-footer" width="144"height="45" />
+                              </Link>
                               <div className="widget-body">
-                                  <p className="widget-about-title">Got Question? Call us 24/7</p>
-                                  <a href="tel:18005707777" className="widget-about-call">1-800-570-7777</a>
-                                  <p className="widget-about-desc">Register now to get updates on pronot get up icons
-                                      & coupons ster now toon.
+                                  <p className="widget-about-title">سوال دارید؟ 24/7 با ما تماس بگیرید</p>
+                                  <a href="tel:18005707777" className="widget-about-call">0217773637</a>
+                                  <p className="widget-about-desc">اکنون ثبت نام کنید تا به‌روزرسانی‌های مربوط به نمادهای pronot get up را دریافت کنید
+                                      & کوپن استر در حال حاضر تون.
                                   </p>
 
                                   <div className="social-icons social-icons-colored">
-                                      <a href="#" className="social-icon social-facebook w-icon-facebook"></a>
-                                      <a href="#" className="social-icon social-twitter w-icon-twitter"></a>
-                                      <a href="#" className="social-icon social-instagram w-icon-instagram"></a>
-                                      <a href="#" className="social-icon social-youtube w-icon-youtube"></a>
-                                      <a href="#" className="social-icon social-pinterest w-icon-pinterest"></a>
+                                      <Link href="#" className="social-icon social-facebook w-icon-facebook"></Link>
+                                      <Link href="#" className="social-icon social-twitter w-icon-twitter"></Link>
+                                      <Link href="#" className="social-icon social-instagram w-icon-instagram"></Link>
+                                      <Link href="#" className="social-icon social-youtube w-icon-youtube"></Link>
+                                      <Link href="#" className="social-icon social-pinterest w-icon-pinterest"></Link>
                                   </div>
                               </div>
                           </div>
                       </div>
                       <div className="col-lg-3 col-sm-6">
                           <div className="widget">
-                              <h3 className="widget-title">Company</h3>
+                              <h3 className="widget-title">شرکت</h3>
                               <ul className="widget-body">
-                                  <li><a href="about-us.html">About Us</a></li>
-                                  <li><a href="#">Team Member</a></li>
-                                  <li><a href="#">Career</a></li>
-                                  <li><a href="contact-us.html">Contact Us</a></li>
-                                  <li><a href="#">Affilate</a></li>
-                                  <li><a href="#">Order History</a></li>
+                                  <li><Link href="about-us.html">درباره ما</Link></li>
+                                  <li><Link href="#">تیم ما</Link></li>
+                                  <li><Link href="#">حرفه</Link></li>
+                                  <li><Link href="#">تماس با ما</Link></li>
+                                  <li><Link href="#">تاریخچه سفارش ها</Link></li>
                               </ul>
                           </div>
                       </div>
                       <div className="col-lg-3 col-sm-6">
                           <div className="widget">
-                              <h4 className="widget-title">My Account</h4>
+                              <h4 className="widget-title">حساب من</h4>
                               <ul className="widget-body">
-                                  <li><a href="#">Track My Order</a></li>
-                                  <li><a href="cart.html">View Cart</a></li>
-                                  <li><a href="login.html">Sign In</a></li>
-                                  <li><a href="#">Help</a></li>
-                                  <li><a href="wishlist.html">My Wishlist</a></li>
-                                  <li><a href="#">Privacy Policy</a></li>
+                              <li><Link href="about-us.html">درباره ما</Link></li>
+                                  <li><Link href="#">تیم ما</Link></li>
+                                  <li><Link href="#">حرفه</Link></li>
+                                  <li><Link href="#">تماس با ما</Link></li>
+                                  <li><Link href="#">تاریخچه سفارش ها</Link></li>
                               </ul>
                           </div>
                       </div>
                       <div className="col-lg-3 col-sm-6">
                           <div className="widget">
-                              <h4 className="widget-title">Customer Service</h4>
+                              <h4 className="widget-title">خدمات مشتری</h4>
                               <ul className="widget-body">
-                                  <li><a href="#">Payment Methods</a></li>
-                                  <li><a href="#">Money-back guarantee!</a></li>
-                                  <li><a href="#">Product Returns</a></li>
-                                  <li><a href="#">Support Center</a></li>
-                                  <li><a href="#">Shipping</a></li>
-                                  <li><a href="#">Term and Conditions</a></li>
+                              <li><Link href="about-us.html">درباره ما</Link></li>
+                                  <li><Link href="#">تیم ما</Link></li>
+                                  <li><Link href="#">حرفه</Link></li>
+                                  <li><Link href="#">تماس با ما</Link></li>
+                                  <li><Link href="#">تاریخچه سفارش ها</Link></li>
                               </ul>
                           </div>
-                      </div>
-                  </div>
-              </div>
-              <div className="footer-middle">
-                  <div className="widget widget-category">
-                      <div className="category-box">
-                          <h6 className="category-name">Consumer Electric:</h6>
-                          <a href="#">TV Television</a>
-                          <a href="#">Air Condition</a>
-                          <a href="#">Refrigerator</a>
-                          <a href="#">Washing Machine</a>
-                          <a href="#">Audio Speaker</a>
-                          <a href="#">Security Camera</a>
-                          <a href="#">View All</a>
-                      </div>
-                      <div className="category-box">
-                          <h6 className="category-name">Clothing & Apparel:</h6>
-                          <a href="#">Men's T-shirt</a>
-                          <a href="#">Dresses</a>
-                          <a href="#">Men's Sneacker</a>
-                          <a href="#">Leather Backpack</a>
-                          <a href="#">Watches</a>
-                          <a href="#">Jeans</a>
-                          <a href="#">Sunglasses</a>
-                          <a href="#">Boots</a>
-                          <a href="#">Rayban</a>
-                          <a href="#">Acccessories</a>
-                      </div>
-                      <div className="category-box">
-                          <h6 className="category-name">Home, Garden & Kitchen:</h6>
-                          <a href="#">Sofa</a>
-                          <a href="#">Chair</a>
-                          <a href="#">Bed Room</a>
-                          <a href="#">Living Room</a>
-                          <a href="#">Cookware</a>
-                          <a href="#">Utensil</a>
-                          <a href="#">Blender</a>
-                          <a href="#">Garden Equipments</a>
-                          <a href="#">Decor</a>
-                          <a href="#">Library</a>
-                      </div>
-                      <div className="category-box">
-                          <h6 className="category-name">Health & Beauty:</h6>
-                          <a href="#">Skin Care</a>
-                          <a href="#">Body Shower</a>
-                          <a href="#">Makeup</a>
-                          <a href="#">Hair Care</a>
-                          <a href="#">Lipstick</a>
-                          <a href="#">Perfume</a>
-                          <a href="#">View all</a>
-                      </div>
-                      <div className="category-box">
-                          <h6 className="category-name">Jewelry & Watches:</h6>
-                          <a href="#">Necklace</a>
-                          <a href="#">Pendant</a>
-                          <a href="#">Diamond Ring</a>
-                          <a href="#">Silver Earing</a>
-                          <a href="#">Leather Watcher</a>
-                          <a href="#">Rolex</a>
-                          <a href="#">Gucci</a>
-                          <a href="#">Australian Opal</a>
-                          <a href="#">Ammolite</a>
-                          <a href="#">Sun Pyrite</a>
-                      </div>
-                      <div className="category-box">
-                          <h6 className="category-name">Computer & Technologies:</h6>
-                          <a href="#">Laptop</a>
-                          <a href="#">iMac</a>
-                          <a href="#">Smartphone</a>
-                          <a href="#">Tablet</a>
-                          <a href="#">Apple</a>
-                          <a href="#">Asus</a>
-                          <a href="#">Drone</a>
-                          <a href="#">Wireless Speaker</a>
-                          <a href="#">Game Controller</a>
-                          <a href="#">View all</a>
                       </div>
                   </div>
               </div>
               <div className="footer-bottom">
                   <div className="footer-left">
-                      <p className="copyright">Copyright © 2021 Wolmart Store. All Rights Reserved.</p>
-                  </div>
-                  <div className="footer-right">
-                      <span className="payment-label mr-lg-8">We're using safe payment for</span>
-                      <figure className="payment">
-                          <img src="assets/images/payment.png" alt="payment" width="159" height="25" />
-                      </figure>
+                      <p className="copyright">حق چاپ © 2021 فروشگاه نیباک. تمامی حقوق محفوظ است.</p>
                   </div>
               </div>
           </div>
