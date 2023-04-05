39509d427c4ccf8a5271bf8e9cf1dc1078523778
amirreza79ahmadi <amirreza79ahmadi@gmail.com>
2023-04-05 15:33:38 +0330

    complete componet single banner

--- components/home/banner/single-banner.jsx
+++ components/home/banner/single-banner.jsx
@@ -1,28 +1,26 @@
 import React from 'react'
-
+import Link from 'next/link'
 export default function Banner() {
   return (
     <div className='container'>
         <div className="sale-banner banner br-sm">
             <div className="banner-content">
-                <h4
-                    className="content-left banner-subtitle text-uppercase mb-8 mb-md-0 mr-0 mr-md-4 text-secondary ls-25">
-                    <span className="text-dark font-weight-bold lh-1 ls-normal">Up
-                        <br/>To</span>20% Sale!</h4>
                 <div className="content-right">
+                    <Link href="#" className="btn btn-white btn-rounded">
+                        همین الان خرید کن
+                        <i className="w-icon-long-arrow-left"></i>
+                    </Link>
                     <h3 className="banner-title text-uppercase font-weight-normal mb-4 mb-md-0 ls-25 text-white">
-                        <span>Pay Only For
-                            <strong className="mr-10 pr-lg-10">Your Lovling Electronics</strong>
-                            Pay Only For
-                            <strong className="mr-10 pr-lg-10">Your Lovling Electronics</strong>
-                            Pay Only For
-                            <strong className="mr-10 pr-lg-10">Your Lovling Electronics</strong>
+                        <span>
+                            <strong className="mr-10 pr-lg-10">لوازم الکترونیکی دوست داشتنی شما فقط برای پرداخت</strong>
+                            <strong className="mr-10 pr-lg-10">لوازم الکترونیکی دوست داشتنی شما فقط برای پرداخت</strong>
+                            <strong className="mr-10 pr-lg-10">لوازم الکترونیکی دوست داشتنی شما فقط برای پرداخت</strong>
                         </span>
                     </h3>
-                    <a href="demo9-shop.html" className="btn btn-white btn-rounded">Shop Now
-                        <i className="w-icon-long-arrow-right"></i>
-                    </a>
                 </div>
+                <h4 className="content-left banner-subtitle text-uppercase mb-8 mb-md-0 mr-0 mr-md-4 text-secondary ls-25 rtl">
+                    <span className="text-dark font-weight-bold lh-1 ls-normal">تا</span>20 درصد فروش!
+                </h4>
             </div>
         </div>
     </div>
