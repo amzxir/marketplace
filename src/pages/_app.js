0a27097d251a36348f1a5ce0ee5a01edf4571459
amirreza79ahmadi <amirreza79ahmadi@gmail.com>
2023-04-15 12:52:33 +0330

    component contact us

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,6 +1,7 @@
 import '@/styles/globals.css'
 import '@/styles/about.css'
- import {AppWrapper} from '../context/state'
+import '@/styles/contact.css'
+import {AppWrapper} from '../context/state'
 
 export default function App({ Component, pageProps }) {
   return (
